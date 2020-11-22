7157b9a950f0cd0bc229f858a03cca5d5da4d4df
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2020-11-22 11:55:40 +0530

    Creating a Custom Exception

--- Backend/springboot-employee-backend/src/main/java/com/chavannikhil802/springboot/repository/EmployeeRepository.java
+++ Backend/springboot-employee-backend/src/main/java/com/chavannikhil802/springboot/repository/EmployeeRepository.java
@@ -1,5 +1,12 @@
 package com.chavannikhil802.springboot.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.chavannikhil802.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+	// JpaRepository provides many of the method implementation used for Database Manipulation
 
 }
