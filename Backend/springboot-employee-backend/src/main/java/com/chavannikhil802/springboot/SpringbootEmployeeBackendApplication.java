29003251e15e204edb68a284dc7be91a9ac71faa
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2020-11-22 11:34:48 +0530

    Initial Commit. Created Backend Folder

---
+++ Backend/springboot-employee-backend/src/main/java/com/chavannikhil802/springboot/SpringbootEmployeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chavannikhil802.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmployeeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEmployeeBackendApplication.class, args);
+	}
+
+}
