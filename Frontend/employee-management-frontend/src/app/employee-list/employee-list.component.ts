9da06b16ad2a61e7946ef38828f48fbad871fc77
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2020-11-23 11:31:39 +0530

    Adding table and dummy data to Employee-List Component

--- Frontend/employee-management-frontend/src/app/employee-list/employee-list.component.ts
+++ Frontend/employee-management-frontend/src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
+  // employees will be used to display the data in employee-list.component.html
+  employees: Employee[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.employees = [{
+      "id" : 1,
+      "firstName" : "Nikhil",
+      "lastName" : "Chavan",
+      "emailId" : "nikhil@gmail.com"
+    },
+    {
+      "id" : 2,
+      "firstName" : "Neha",
+      "lastName" : "Chavan",
+      "emailId" : "neha@gmail.com"
+    },
+    {
+      "id" : 3,
+      "firstName" : "Narendra",
+      "lastName" : "Chavan",
+      "emailId" : "narendra@gmail.com"
+    }];
   }
 
 }
