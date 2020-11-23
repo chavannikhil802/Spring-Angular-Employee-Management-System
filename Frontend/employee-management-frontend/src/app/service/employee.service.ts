a09e65bbccb700606cdbb6d44e2a4b33877f31aa
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2020-11-23 11:58:26 +0530

    Writing an employee service method to fetch the list of the employees from the database

--- Frontend/employee-management-frontend/src/app/service/employee.service.ts
+++ Frontend/employee-management-frontend/src/app/service/employee.service.ts
@@ -1,10 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
+  private baseUrl = "http://localhost:8080/api/v1/employees";
+
   constructor(private httpClient : HttpClient) { }
+
+  // Method to fetch the list of Employees from the database
+  getEmployeesList() : Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(`${this.baseUrl}`);
+  }
 }
