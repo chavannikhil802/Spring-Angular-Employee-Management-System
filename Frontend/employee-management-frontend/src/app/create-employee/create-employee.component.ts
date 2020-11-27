eb787b97ab50a0dc47b8c33b5aa9aa89fb182f1b
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2020-11-27 12:38:52 +0530

    Updating Comments in createEmployeeEcomponent.ts

--- Frontend/employee-management-frontend/src/app/create-employee/create-employee.component.ts
+++ Frontend/employee-management-frontend/src/app/create-employee/create-employee.component.ts
@@ -11,24 +11,30 @@ import { EmployeeService } from '../service/employee.service';
 export class CreateEmployeeComponent implements OnInit {
 
   employee : Employee = new Employee();
+  
+  // Constructor Injection of EmployeeService and Router
   constructor(private employeeService : EmployeeService,
               private router : Router) { }
 
   ngOnInit(): void {
   }
 
+  // Method call to save the Employee
   saveEmployee() {
     this.employeeService.createEmployee(this.employee).subscribe( data => {
       console.log(data);
+      // Method call to redirect to Employee List Page
       this.goToEmployeeList();
     },
     error => console.log(error));
   }
 
+  // Method to redirect to Employee List Page
   goToEmployeeList() {
     this.router.navigate(['/employees']);
   }
 
+  // When the user clicks Submit button, onSubmit() will execute
   onSubmit() {
     this.saveEmployee();
     console.log(this.employee);
